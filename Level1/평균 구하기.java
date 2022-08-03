44098a8978527942441101dee842b7846fe11fe3
yeahzxnn <102504021+yeahzxnn@users.noreply.github.com>
2022-08-04 00:00:02 +0900

    Create 평균 구하기.java

---
+++ Level1/평균 구하기.java
@@ -0,0 +1,14 @@
+//배열 평균 구하기
+class Solution {
+    public double solution(int[] arr) {
+        
+        int sum = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        
+        double answer = (double) sum/arr.length;
+        return answer;
+    }
+}
