07cf3009ba87aa75a0a24a1df36b1d97e1741942
yeahzxnn <102504021+yeahzxnn@users.noreply.github.com>
2022-08-05 00:57:52 +0900

    Create 하샤드 수.java

---
+++ Level1/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+       int num = x;
+       int sum = 0;
+        
+        while(num!=0){
+            sum+=num%10;
+            num/=10;
+        }
+        return x%sum == 0? true : false;
+    }
+}
