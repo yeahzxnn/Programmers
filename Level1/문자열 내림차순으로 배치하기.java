4f57095f86a219f08a0d81315a10b7db4b87e2b5
yeahzxnn <102504021+yeahzxnn@users.noreply.github.com>
2022-08-22 11:30:52 +0900

    Create 문자열 내림차순으로 배치하기.java

---
+++ Level1/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        char arr[] = s.toCharArray();
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder(new String(arr,0,arr.length));
+        return sb.reverse().toString();
+    }
+}
