0f91c92066b85637e3a699a5f4f914eb6d87c7ca
yeahzxnn <102504021+yeahzxnn@users.noreply.github.com>
2022-09-08 11:02:06 +0900

    Update 정수 제곱근 판별.java

--- Level1/정수 제곱근 판별.java
+++ Level1/정수 제곱근 판별.java
@@ -7,3 +7,13 @@ class Solution {
         }else return -1;
     }
 }
+
+//double형으로 바꾸지 않고도 할 수 있는 방법
+class Solution {
+    public long solution(long n) {
+        if(Math.pow((int)Math.sqrt(n),2) == n) {
+            return (long) Math.pow(Math.sqrt(n)+1,2);
+        }
+        return -1;
+    }
+}
